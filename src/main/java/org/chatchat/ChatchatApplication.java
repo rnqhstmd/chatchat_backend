c3efe84ecf3b7d883a20a49ceff13c8dae46fb9a
BonSeung <bs.koo@sqisoft.com>
2024-09-04 14:36:10 +0900

    init: Spring Initializing - #1

---
+++ src/main/java/org/chatchat/ChatchatApplication.java
@@ -0,0 +1,13 @@
+package org.chatchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatchatApplication.class, args);
+	}
+
+}
