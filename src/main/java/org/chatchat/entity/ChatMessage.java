7fd6058855ee8d38fd1d2dafbe4e041775084847
BonSeung <bs.koo@sqisoft.com>
2024-09-06 17:07:18 +0900

    feat: Getter, Builder 어노테이션 추가 - #6

--- src/main/java/org/chatchat/entity/ChatMessage.java
+++ src/main/java/org/chatchat/entity/ChatMessage.java
@@ -1,18 +1,21 @@
 package org.chatchat.entity;
 
 import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
-import java.time.ZonedDateTime;
 
+import java.time.Instant;
+
+@Getter
+@Builder
 @Document(collection = "chat_messages")
 public class ChatMessage {
 
     @Id
     private String id;
-
     private String chatRoomId;
     private String senderId;
     private String content;
-    private ZonedDateTime sentAt;
-
+    private Instant sentAt;
 }
