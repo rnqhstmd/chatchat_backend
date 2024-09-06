38db64160571184c7f20c08333e6c59b6a82731b
BonSeung <bs.koo@sqisoft.com>
2024-09-06 17:08:28 +0900

    refactor: 필요없는 임포트 제거  - #6

--- src/main/java/org/chatchat/repository/ChatPartRepository.java
+++ src/main/java/org/chatchat/repository/ChatPartRepository.java
@@ -1,7 +1,6 @@
 package org.chatchat.repository;
 
 import org.chatchat.entity.ChatPart;
-import org.chatchat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatPartRepository extends JpaRepository<ChatPart, Long> {
--- src/main/java/org/chatchat/repository/ChatRoomRepository.java
+++ src/main/java/org/chatchat/repository/ChatRoomRepository.java
@@ -1,7 +1,6 @@
 package org.chatchat.repository;
 
 import org.chatchat.entity.ChatRoom;
-import org.chatchat.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
