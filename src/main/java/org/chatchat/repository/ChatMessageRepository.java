29b6f4ea6a33d585d185a445eca9acd15f02e553
BonSeung <bs.koo@sqisoft.com>
2024-09-06 17:08:02 +0900

    feat: MongoRepository 대신 동적 쿼리 사용 위해 MongoTemplate 사용 - #6

--- src/main/java/org/chatchat/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-package org.chatchat.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ChatMessageRepository extends MongoRepository<ChatMessageRepository, String> {
-
-}
