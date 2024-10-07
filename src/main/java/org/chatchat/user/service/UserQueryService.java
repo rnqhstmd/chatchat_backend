b7a4f0c63c57dd854e9dcd63e12fd89e538aa764
BonSeung <bs.koo@sqisoft.com>
2024-10-07 14:41:34 +0900

    feat: User 쿼리용 서비스 추가 -#10

---
+++ src/main/java/org/chatchat/user/service/UserQueryService.java
@@ -0,0 +1,39 @@
+package org.chatchat.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.chatchat.common.exception.ConflictException;
+import org.chatchat.common.exception.NotFoundException;
+import org.chatchat.user.domain.User;
+import org.chatchat.user.domain.UserRepository;
+import org.springframework.stereotype.Service;
+
+import static org.chatchat.common.exception.type.ErrorType.*;
+
+@Service
+@RequiredArgsConstructor
+public class UserQueryService {
+
+    private final UserRepository userRepository;
+
+    public User findExistingUserByName(String userName) {
+        return userRepository.findByName(userName)
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_ERROR));
+    }
+
+    public User findExistingUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_ERROR));
+    }
+
+    public void validateIsDuplicatedEmail(String email) {
+        if (!userRepository.existsByEmail(email)) {
+            throw  new ConflictException(DUPLICATED_EMAIL_ERROR);
+        }
+    }
+
+    public void validateIsDuplicatedName(String name) {
+        if (!userRepository.existsByName(name)) {
+            throw  new ConflictException(DUPLICATED_NAME_ERROR);
+        }
+    }
+}
