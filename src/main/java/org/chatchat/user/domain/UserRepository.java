38b3eec647db4726af13d488b40298806577f573
BonSeung <bs.koo@sqisoft.com>
2024-10-07 14:42:03 +0900

    feat: User 객체 검증용 메서드 추가 -#10

--- src/main/java/org/chatchat/user/domain/UserRepository.java
+++ src/main/java/org/chatchat/user/domain/UserRepository.java
@@ -1,8 +1,16 @@
 package org.chatchat.user.domain;
 
-import org.chatchat.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findByName(String name);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByName(String name);
 }
