8dfa3d561525659173a4f3358891b45a1803cb0a
BonSeung <bs.koo@sqisoft.com>
2024-10-08 09:19:12 +0900

    feat: 인증되지 않은 사용자 접근 막는 엔드포인트 적용 - #12

--- src/main/java/org/chatchat/common/config/SecurityConfig.java
+++ src/main/java/org/chatchat/common/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package org.chatchat.common.config;
 
 
+import lombok.RequiredArgsConstructor;
+import org.chatchat.security.auth.handler.CustomAuthenticationEntryPoint;
+import org.chatchat.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,16 +12,18 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private static final String[] WHITE_LIST = {
-            "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/", "/api/**", "/error/**", "/swagger", "/swagger/**"
+            "/v3/api-docs/**", "/swagger-ui/**", "/swagger-resources/**", "/", "/api/auth/sign-up", "/error/**", "/swagger", "/swagger/**"
     };
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -28,8 +33,11 @@ public class SecurityConfig {
                         .requestMatchers(WHITE_LIST).permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults());
+                .exceptionHandling(exceptionHandler -> exceptionHandler
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                );
 
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
