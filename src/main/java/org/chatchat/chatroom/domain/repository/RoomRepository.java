1a5a29ff5a5b9ffeaaf9f8f4459e699d9677d073
BonSeung <bs.koo@sqisoft.com>
2024-10-10 10:54:07 +0900

    rename: 클래스 이름 변경 - #2 - ChatRoomRepository -> RoomRepository

--- src/main/java/org/chatchat/chatroom/domain/repository/RoomRepository.java
+++ src/main/java/org/chatchat/chatroom/domain/repository/RoomRepository.java
@@ -3,6 +3,6 @@ package org.chatchat.chatroom.domain.repository;
 import org.chatchat.chatroom.domain.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChatRoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends JpaRepository<Room, Long> {
 
 }
