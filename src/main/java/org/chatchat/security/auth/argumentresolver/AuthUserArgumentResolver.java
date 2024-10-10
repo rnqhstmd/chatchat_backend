39b1703c4451151675bc281a0ddad6cc78385d1c
BonSeung <bs.koo@sqisoft.com>
2024-10-10 10:59:25 +0900

    feat: 커스텀 어노테이션을 위한 ArgumentResolver 추가 - #16

---
+++ src/main/java/org/chatchat/security/auth/argumentresolver/AuthUserArgumentResolver.java
@@ -0,0 +1,37 @@
+package org.chatchat.security.auth.argumentresolver;
+
+import org.chatchat.common.exception.UnauthorizedException;
+import org.chatchat.security.auth.annotation.AuthUser;
+import org.chatchat.security.auth.dto.response.UserPrincipal;
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import static org.chatchat.common.exception.type.ErrorType.NO_AUTHORIZATION_ERROR;
+
+public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthUser.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.getPrincipal() instanceof UserPrincipal) {
+            UserPrincipal principal = (UserPrincipal) authentication.getPrincipal();
+            return principal.getUser();
+        }
+
+        throw new UnauthorizedException(NO_AUTHORIZATION_ERROR);
+    }
+}
