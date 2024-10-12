eea4d432dabfa15f9448bf861a7ecf250af0171d
BonSeung <bs.koo@sqisoft.com>
2024-10-12 23:35:53 +0900

    feat: 채팅방 참가 API 추가 - #19

--- src/main/java/org/chatchat/chatroom/controller/RoomController.java
+++ src/main/java/org/chatchat/chatroom/controller/RoomController.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.chatchat.chatroom.dto.request.CreateRoomRequest;
 import org.chatchat.chatroom.service.RoomService;
+import org.chatchat.security.auth.annotation.AuthUser;
+import org.chatchat.user.domain.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,13 @@ public class RoomController {
     @PostMapping
     public ResponseEntity<Void> createRoom(@RequestBody @Valid CreateRoomRequest createRoomRequest) {
         roomService.createChatRoom(createRoomRequest);
+        return ResponseEntity.ok().build();
+    }
+
+    // 채팅방 참가
+    @PostMapping("/{roomId}/join")
+    public ResponseEntity<Void> joinRoom(@PathVariable("roomId") Long roomId, @AuthUser User user) {
+        roomService.joinRoom(roomId, user);
         return ResponseEntity.ok().build();
     }
 }
