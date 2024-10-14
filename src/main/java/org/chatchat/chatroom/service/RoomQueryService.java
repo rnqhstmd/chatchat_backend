a5210a419302c594e270a551304171858d786e8e
BonSeung <bs.koo@sqisoft.com>
2024-10-14 17:53:24 +0900

    feat: 채팅방 전체 조회 기능 추가 - #19

--- src/main/java/org/chatchat/chatroom/service/RoomQueryService.java
+++ src/main/java/org/chatchat/chatroom/service/RoomQueryService.java
@@ -3,9 +3,12 @@ package org.chatchat.chatroom.service;
 import lombok.RequiredArgsConstructor;
 import org.chatchat.chatroom.domain.Room;
 import org.chatchat.chatroom.domain.repository.RoomRepository;
+import org.chatchat.chatroom.dto.response.RoomInfoResponse;
 import org.chatchat.common.exception.ConflictException;
 import org.chatchat.common.exception.NotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 import static org.chatchat.common.exception.type.ErrorType.DUPLICATED_ROOM_NAME_ERROR;
 import static org.chatchat.common.exception.type.ErrorType.ROOM_NOT_FOUND_ERROR;
@@ -15,6 +18,12 @@ import static org.chatchat.common.exception.type.ErrorType.ROOM_NOT_FOUND_ERROR;
 public class RoomQueryService {
 
     private final RoomRepository roomRepository;
+
+    public List<RoomInfoResponse> getAllRooms() {
+        return roomRepository.findAll().stream()
+                .map(room -> new RoomInfoResponse(room.getId(), room.getName()))
+                .toList();
+    }
 
     public Room findExistingRoomById(Long roomId) {
         return roomRepository.findById(roomId)
