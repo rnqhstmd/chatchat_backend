a380feedbb1b64a80217ff6faa36a0d33adc696e
BonSeung <bs.koo@sqisoft.com>
2024-10-14 15:19:27 +0900

    feat: 채팅 관련 API 추가 - #19

---
+++ src/main/java/org/chatchat/chatmessage/controller/ChatMessageController.java
@@ -0,0 +1,60 @@
+package org.chatchat.chatmessage.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.chatchat.chatmessage.domain.ChatMessage;
+import org.chatchat.chatmessage.dto.MessageRequest;
+import org.chatchat.chatmessage.service.ChatMessageService;
+import org.chatchat.chatmessage.util.WebSocketEventListener;
+import org.chatchat.security.auth.annotation.AuthUser;
+import org.chatchat.user.domain.User;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatMessageController {
+
+    private final ChatMessageService chatMessageService;
+    private final SimpMessageSendingOperations simpMessageSendingOperations;
+    private final WebSocketEventListener webSocketEventListener;
+    private final List<String> channelList = new CopyOnWriteArrayList<>();
+
+    @SubscribeMapping("/user-count")
+    public int getInitialUserCount() {
+        return webSocketEventListener.getTotalSubscriberCount();
+    }
+
+    @SubscribeMapping("/user-list")
+    public Map<String, Set<String>> getInitialUserList() {
+        return webSocketEventListener.getSessionMap();
+    }
+
+    @MessageMapping("/channel-list")
+    public void channel(String channelName) {
+        if (!channelList.contains(channelName)) {
+            channelList.add(channelName);
+        }
+
+        simpMessageSendingOperations.convertAndSend("/sub/room-list", channelList);
+    }
+
+    @SubscribeMapping("/channel-list")
+    public List<String> getInitialChannelList() {
+        return channelList;
+    }
+
+    @MessageMapping("/chat/{roomId}")
+    public void sendMessage(@DestinationVariable Long roomId, @RequestBody MessageRequest messageRequest, @AuthUser User user) {
+        ChatMessage chatMessage = chatMessageService.saveMessage(roomId, messageRequest, user);
+        webSocketEventListener.broadcastMessage(roomId, chatMessage);
+    }
+}
