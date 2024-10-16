1ebb126bfa8d24e58ffa8ff9d4856d9b35010069
BonSeung <bs.koo@sqisoft.com>
2024-10-16 15:19:43 +0900

    feat: 로그아웃 API 추가 - #27

--- src/main/java/org/chatchat/security/auth/controller/AuthController.java
+++ src/main/java/org/chatchat/security/auth/controller/AuthController.java
@@ -1,14 +1,19 @@
 package org.chatchat.security.auth.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.chatchat.security.auth.dto.request.LoginRequest;
 import org.chatchat.security.auth.dto.request.SignupRequest;
 import org.chatchat.security.auth.dto.response.LoginResponse;
 import org.chatchat.security.auth.service.AuthService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static org.chatchat.security.cookie.CookieUtil.addCookie;
+import static org.chatchat.security.cookie.CookieUtil.clearCookies;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -24,8 +29,18 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody LoginRequest loginRequest,
+                                                   HttpServletResponse response) {
         LoginResponse loginResponse = authService.login(loginRequest);
-        return ResponseEntity.ok(loginResponse);
+        addCookie(response, loginResponse.jwtToken());
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, response.getHeader("Set-Cookie"))
+                .body(loginResponse);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(final HttpServletResponse response) {
+        clearCookies(response);
+        return ResponseEntity.ok().build();
     }
 }
