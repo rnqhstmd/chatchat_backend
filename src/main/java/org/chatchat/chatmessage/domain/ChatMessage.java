d8522c18aed3b4ca634f0b6302b1a5d18f1a9f91
BonSeung <bs.koo@sqisoft.com>
2024-10-16 11:00:40 +0900

    feat: MessageType 분리 - #2

--- src/main/java/org/chatchat/chatmessage/domain/ChatMessage.java
+++ src/main/java/org/chatchat/chatmessage/domain/ChatMessage.java
@@ -14,24 +14,9 @@ public class ChatMessage {
 
     @Id
     private String id;
-    public enum MessageType {
-        ENTER, TALK,
-    }
     private MessageType type;
     private String roomId;
     private String sender;
     private String content;
     private Instant sentAt;
-
-    public void setRoomId(String roomId) {
-        this.roomId = roomId;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
