7f9e8f36f406245bd9728b9cf46f3a71ff103ce0
BonSeung <bs.koo@sqisoft.com>
2024-10-16 16:33:39 +0900

    feat: 웹소켓 연결, 해제 시 이벤트 리스너 수정 - #24

--- src/main/java/org/chatchat/chatmessage/util/WebSocketEventListener.java
+++ src/main/java/org/chatchat/chatmessage/util/WebSocketEventListener.java
@@ -5,17 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.chatchat.chatmessage.domain.ChatMessage;
 import org.chatchat.chatmessage.domain.MessageType;
 import org.chatchat.common.exception.InternalServerException;
-import org.chatchat.common.exception.NotFoundException;
 import org.chatchat.common.exception.UnauthorizedException;
 import org.chatchat.common.exception.type.ErrorType;
-import org.chatchat.security.jwt.provider.JwtProvider;
-import org.chatchat.user.domain.User;
-import org.chatchat.user.service.UserQueryService;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
@@ -27,9 +21,6 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 public class WebSocketEventListener {
 
     private final SimpMessageSendingOperations messagingTemplate;
-    private final UserQueryService userQueryService;
-    private final JwtProvider jwtProvider;
-
 
     // 웹소켓 연결 시 호출되는 메서드
     @EventListener
@@ -37,55 +28,37 @@ public class WebSocketEventListener {
         log.info("New WebSocket connection established.");
         log.info("event.getMessage(): {}", event.getMessage());
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap((Message<?>) event.getMessage().getHeaders().get("simpConnectMessage"));
-        String jwtToken = headerAccessor.getFirstNativeHeader("Authorization");
-        log.info("JWT Token 값: {}", jwtToken);
-        if (jwtToken != null && jwtToken.startsWith("Bearer ")) {
-            jwtToken = jwtToken.substring(7);  // JWT 토큰 추출
-            try {
-                // 토큰의 유효성 검증
-                jwtProvider.isValidToken(jwtToken);
-
-                // JWT에서 사용자 정보 추출 및 인증 설정
-                String userEmail = jwtProvider.getUserEmailFromToken(jwtToken);
-                User user = userQueryService.findExistingUserByEmail(userEmail);
-
-                if (user == null && user.getUsername() == null) {
-                    throw new NotFoundException(ErrorType.USER_NOT_FOUND_ERROR);
-                }
-                // 사용자 정보 저장
-                headerAccessor.getSessionAttributes().put("username", user.getUsername());
-                log.info("Authenticated WebSocket connection for user: {}", user.getUsername());
-            } catch (UnauthorizedException u) {
-                SimpMessageHeaderAccessor accessor = SimpMessageHeaderAccessor.create(SimpMessageType.DISCONNECT);
-                accessor.setSessionId(headerAccessor.getSessionId());
-                messagingTemplate.convertAndSend("/app/disconnect", "Invalid token, closing connection.");
-                throw new UnauthorizedException(ErrorType.TOKEN_MALFORMED_ERROR, u.getMessage());
-            } catch (Exception e) {
-                throw new InternalServerException(ErrorType.INTERNAL_SERVER_ERROR, e.getMessage());
+        try {
+            String username = (String) headerAccessor.getSessionAttributes().get("username");
+            if (username == null) {
+                throw new UnauthorizedException(ErrorType.NO_AUTHORIZATION_ERROR, "세션에 username 이 비어있습니다.");
             }
+        } catch (Exception e) {
+            throw new InternalServerException(ErrorType.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 
     // 웹소켓 연결 해제 시 호출되는 메서드
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
-        Long roomId = (Long) headerAccessor.getSessionAttributes().get("roomId");
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap((Message<?>) event.getMessage().getHeaders().get("simpConnectMessage"));
+        try {
+            String username = (String) headerAccessor.getSessionAttributes().get("username");
+            if (username == null) {
+                throw new UnauthorizedException(ErrorType.NO_AUTHORIZATION_ERROR, "세션에 username 이 비어있습니다.");
+            }
+            Long roomId = (Long) headerAccessor.getSessionAttributes().get("roomId");
 
-        if (username == null) {
-            throw new UnauthorizedException(ErrorType.NO_AUTHORIZATION_ERROR);
+            log.info("User Disconnected: {}", username);
+            ChatMessage chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .roomId(String.valueOf(roomId))
+                    .sender(username)
+                    .content(username + " 님의 연결이 종료되었습니다.")
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        } catch (Exception e) {
+            throw new InternalServerException(ErrorType.INTERNAL_SERVER_ERROR, e.getMessage());
         }
-
-        log.info("User Disconnected: {}", username);
-        // 연결 해제 시 사용자 퇴장 메시지 전송
-        ChatMessage chatMessage = ChatMessage.builder()
-                .type(MessageType.LEAVE)
-                .roomId(String.valueOf(roomId))
-                .sender(username)
-                .content(username + " has left the chat")
-                .build();
-
-        messagingTemplate.convertAndSend("/topic/public", chatMessage);
     }
 }
