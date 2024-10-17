c8a812fd83884b7824b182f10777f7aa018eb9c2
BonSeung <bs.koo@sqisoft.com>
2024-10-17 13:53:14 +0900

    feat: 채팅방 전체 조회 시 페이지네이션 적용 - #28

--- src/main/java/org/chatchat/chatroom/service/RoomQueryService.java
+++ src/main/java/org/chatchat/chatroom/service/RoomQueryService.java
@@ -6,6 +6,10 @@ import org.chatchat.chatroom.domain.repository.RoomRepository;
 import org.chatchat.chatroom.dto.response.RoomInfoResponse;
 import org.chatchat.common.exception.ConflictException;
 import org.chatchat.common.exception.NotFoundException;
+import org.chatchat.common.page.dto.request.PageRequestDto;
+import org.chatchat.common.page.dto.response.PageResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,10 +22,16 @@ public class RoomQueryService {
 
     private final RoomRepository roomRepository;
 
-    public List<RoomInfoResponse> getAllRooms() {
-        return roomRepository.findAll().stream()
+    public PageResponseDto<RoomInfoResponse> getAllRooms(int page) {
+        PageRequestDto pageRequestDto = new PageRequestDto(page);
+        Pageable pageable = pageRequestDto.toRoomPageable();
+
+        Page<Room> roomPage = roomRepository.findAll(pageable);
+        List<RoomInfoResponse> roomResponses = roomPage.getContent().stream()
                 .map(room -> new RoomInfoResponse(room.getId(), room.getName()))
                 .toList();
+
+        return PageResponseDto.of(roomResponses, roomPage.getNumber(), roomPage.getTotalPages());
     }
 
     public Room findExistingRoomById(Long roomId) {
