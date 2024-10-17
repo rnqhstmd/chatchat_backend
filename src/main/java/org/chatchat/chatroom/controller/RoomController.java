e0b8956bc5ff5cb7b9fc720994d41f0a069a6c76
BonSeung <bs.koo@sqisoft.com>
2024-10-17 13:36:47 +0900

    feat: page 정보 받는 파라미터 추가 - #28

--- src/main/java/org/chatchat/chatroom/controller/RoomController.java
+++ src/main/java/org/chatchat/chatroom/controller/RoomController.java
@@ -8,6 +8,7 @@ import org.chatchat.chatroom.dto.request.CreateRoomRequest;
 import org.chatchat.chatroom.dto.response.RoomInfoResponse;
 import org.chatchat.chatroom.service.RoomQueryService;
 import org.chatchat.chatroom.service.RoomService;
+import org.chatchat.common.page.dto.response.PageResponseDto;
 import org.chatchat.security.auth.annotation.AuthUser;
 import org.chatchat.user.domain.User;
 import org.springframework.http.ResponseEntity;
@@ -38,15 +39,18 @@ public class RoomController {
 
     // 채팅방 참가
     @PostMapping("/{roomId}/join")
-    public ResponseEntity<Void> joinRoom(@PathVariable("roomId") Long roomId, @AuthUser User user) {
+    public ResponseEntity<Void> joinRoom(@PathVariable("roomId") Long roomId,
+                                         @AuthUser User user) {
         roomService.joinRoom(roomId, user);
         return ResponseEntity.ok().build();
     }
 
     // 채팅방 입장 및 이전 메시지 불러오기
     @GetMapping("/{roomId}/enter")
-    public ResponseEntity<List<MessageResponse>> enterRoom(@PathVariable("roomId") Long roomId, @AuthUser User user) {
-        List<MessageResponse> messages = chatMessageQueryService.loadMessagesByRoomId(roomId, user.getId());
+    public ResponseEntity<PageResponseDto<MessageResponse>> enterRoom(@PathVariable("roomId") Long roomId,
+                                                                      @RequestParam(defaultValue = "0") int page,
+                                                                      @AuthUser User user) {
+        PageResponseDto<MessageResponse> messages = chatMessageQueryService.loadMessagesByRoomId(roomId, user.getId(), page);
         return ResponseEntity.ok(messages);
     }
 }
