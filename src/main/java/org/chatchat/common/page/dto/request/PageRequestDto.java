9815793887ab7e5d373828482b72749db2f5e1a8
BonSeung <bs.koo@sqisoft.com>
2024-10-17 13:48:47 +0900

    feat: 채팅방용 페이지네이션 메서드 추가 - #28

--- src/main/java/org/chatchat/common/page/dto/request/PageRequestDto.java
+++ src/main/java/org/chatchat/common/page/dto/request/PageRequestDto.java
@@ -4,6 +4,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 public record PageRequestDto(int page) {
+
+    private static final int ROOM_PAGE_SIZE = 10;
     private static final int MESSAGE_PAGE_SIZE = 20;
 
     public PageRequestDto(int page) {
@@ -11,8 +13,11 @@ public record PageRequestDto(int page) {
     }
 
     public Pageable toMessagePageable() {
-        // Pageable 객체 생성
         return PageRequest.of(page, MESSAGE_PAGE_SIZE);
+    }
+
+    public Pageable toRoomPageable() {
+        return PageRequest.of(page, ROOM_PAGE_SIZE);
     }
 }
 
