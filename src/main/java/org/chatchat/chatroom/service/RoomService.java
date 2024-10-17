125151a670e232729e9e7caa5fdc114e681be3e7
BonSeung <bs.koo@sqisoft.com>
2024-10-17 16:24:26 +0900

    feat: 채팅방 초대 기능 추가 - #31

--- src/main/java/org/chatchat/chatroom/service/RoomService.java
+++ src/main/java/org/chatchat/chatroom/service/RoomService.java
@@ -7,12 +7,12 @@ import org.chatchat.chatpart.service.ChatPartService;
 import org.chatchat.chatroom.domain.Room;
 import org.chatchat.chatroom.domain.repository.RoomRepository;
 import org.chatchat.chatroom.dto.request.CreateRoomRequest;
-import org.chatchat.common.exception.ConflictException;
+import org.chatchat.chatroom.dto.request.InviteUserToRoomRequest;
 import org.chatchat.user.domain.User;
+import org.chatchat.user.service.UserQueryService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.chatchat.common.exception.type.ErrorType.ALREADY_JOINED_USER_ERROR;
 
 @Service
 @Transactional
@@ -23,7 +23,11 @@ public class RoomService {
     private final RoomQueryService roomQueryService;
     private final ChatPartService chatPartService;
     private final ChatPartQueryService chatPartQueryService;
+    private final UserQueryService userQueryService;
 
+    /**
+     * 채팅방 생성
+     */
     public void createChatRoom(CreateRoomRequest createRoomRequest) {
         String name = createRoomRequest.name();
         roomQueryService.validateExistingRoomByName(name);
@@ -31,12 +35,16 @@ public class RoomService {
         roomRepository.save(room);
     }
 
-    public void joinRoom(Long roomId, User user) {
+    /**
+     * 채팅방 초대
+     */
+    public void inviteUserToRoom(Long roomId, InviteUserToRoomRequest inviteUserToRoomRequest) {
         Room room = roomQueryService.findExistingRoomById(roomId);
-        chatPartQueryService.validateUserRoomMember(roomId, user.getId());
+        User inviteUser = userQueryService.findExistingUserByName(inviteUserToRoomRequest.username());
+        // 이미 채팅방에 참여 중인 유저 검증
+        chatPartQueryService.isUserMemberOfRoom(roomId, inviteUser.getId());
 
-        // 유저가 채팅방에 참여하도록 ChatPart 생성
-        ChatPart chatPart = new ChatPart(room, user);
+        ChatPart chatPart = new ChatPart(room, inviteUser);
         chatPartService.saveChatPart(chatPart);
     }
 }
