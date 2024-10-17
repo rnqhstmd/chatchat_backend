3ba88ca317f95f8cbdc1d5a8db954c947ee39ce9
BonSeung <bs.koo@sqisoft.com>
2024-10-17 17:07:10 +0900

    feat: 채팅방 멤버가 아닌 예외 추가 - #34

--- src/main/java/org/chatchat/common/exception/type/ErrorType.java
+++ src/main/java/org/chatchat/common/exception/type/ErrorType.java
@@ -33,6 +33,7 @@ public enum ErrorType {
     ROOM_NOT_FOUND_ERROR("ROOM_40400", "채팅방을 찾을 수 없습니다."),
     DUPLICATED_ROOM_NAME_ERROR("ROOM_40900", "이미 사용 중인 채팅방 이름입니다."),
     ALREADY_JOINED_USER_ERROR("ROOM_40901", "이미 참가한 채팅방입니다."),
+    NOT_ROOM_MEMBER_ERROR("ROOM_40300", "채팅방의 멤버가 아닙니다."),
 
     // Resource
     NO_RESOURCE_ERROR( "RESOURCE_40000", "해당 리소스를 찾을 수 없습니다."),
