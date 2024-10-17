7cbcfe0b27cfd593695d3d66cb8126da6887b892
BonSeung <bs.koo@sqisoft.com>
2024-10-17 16:23:54 +0900

    feat: 이메일로 유저 찾는 메서드 추가 - #31

--- src/main/java/org/chatchat/user/service/UserQueryService.java
+++ src/main/java/org/chatchat/user/service/UserQueryService.java
@@ -33,15 +33,20 @@ public class UserQueryService {
                 .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_ERROR));
     }
 
+    public User findExistingUserByName(String name) {
+        return userRepository.findByUsername(name)
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND_ERROR));
+    }
+
     public void validateIsDuplicatedEmail(String email) {
         if (userRepository.existsByEmail(email)) {
-            throw  new ConflictException(DUPLICATED_EMAIL_ERROR);
+            throw new ConflictException(DUPLICATED_EMAIL_ERROR);
         }
     }
 
     public void validateIsDuplicatedName(String name) {
         if (userRepository.existsByUsername(name)) {
-            throw  new ConflictException(DUPLICATED_NAME_ERROR);
+            throw new ConflictException(DUPLICATED_NAME_ERROR);
         }
     }
 }
