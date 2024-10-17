7182ff8e6e50e3d36000ef08387b65f8b4e2ee36
BonSeung <bs.koo@sqisoft.com>
2024-10-17 10:02:57 +0900

    feat: 채팅방 참가 시 이미 참가했던 적이 있는지 검증 추가 - #28

--- src/main/java/org/chatchat/chatroom/service/RoomService.java
+++ src/main/java/org/chatchat/chatroom/service/RoomService.java
@@ -2,16 +2,17 @@ package org.chatchat.chatroom.service;
 
 import lombok.RequiredArgsConstructor;
 import org.chatchat.chatpart.domain.ChatPart;
+import org.chatchat.chatpart.service.ChatPartQueryService;
 import org.chatchat.chatpart.service.ChatPartService;
 import org.chatchat.chatroom.domain.Room;
 import org.chatchat.chatroom.domain.repository.RoomRepository;
 import org.chatchat.chatroom.dto.request.CreateRoomRequest;
-import org.chatchat.common.exception.NotFoundException;
+import org.chatchat.common.exception.ConflictException;
 import org.chatchat.user.domain.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.chatchat.common.exception.type.ErrorType.ROOM_NOT_FOUND_ERROR;
+import static org.chatchat.common.exception.type.ErrorType.ALREADY_JOINED_USER_ERROR;
 
 @Service
 @Transactional
@@ -21,6 +22,7 @@ public class RoomService {
     private final RoomRepository roomRepository;
     private final RoomQueryService roomQueryService;
     private final ChatPartService chatPartService;
+    private final ChatPartQueryService chatPartQueryService;
 
     public void createChatRoom(CreateRoomRequest createRoomRequest) {
         String name = createRoomRequest.name();
@@ -30,8 +32,12 @@ public class RoomService {
     }
 
     public void joinRoom(Long roomId, User user) {
-        Room room = roomRepository.findById(roomId)
-                .orElseThrow(() -> new NotFoundException(ROOM_NOT_FOUND_ERROR));
+        Room room = roomQueryService.findExistingRoomById(roomId);
+
+        // 이미 채팅방에 참가했는지 확인
+        if (chatPartQueryService.validateUserRoomMember(roomId, user.getId())) {
+            throw new ConflictException(ALREADY_JOINED_USER_ERROR);
+        }
 
         // 유저가 채팅방에 참여하도록 ChatPart 생성
         ChatPart chatPart = new ChatPart(room, user);
