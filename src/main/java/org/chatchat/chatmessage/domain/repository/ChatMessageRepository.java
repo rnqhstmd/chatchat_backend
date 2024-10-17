816821a2a34b79e8aeee16840021e723f537e94b
BonSeung <bs.koo@sqisoft.com>
2024-10-17 13:36:08 +0900

    feat: 메세지 조회 메서드 페이징 적용 - #28

--- src/main/java/org/chatchat/chatmessage/domain/repository/ChatMessageRepository.java
+++ src/main/java/org/chatchat/chatmessage/domain/repository/ChatMessageRepository.java
@@ -1,11 +1,12 @@
 package org.chatchat.chatmessage.domain.repository;
 
 import org.chatchat.chatmessage.domain.ChatMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 
-    List<ChatMessage> findByRoomIdOrderBySentAtDesc(String roomId);
+    Page<ChatMessage> findByRoomIdOrderBySentAtDesc(String roomId, Pageable pageable);
 }
