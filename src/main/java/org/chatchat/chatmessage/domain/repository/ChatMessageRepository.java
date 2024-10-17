748b73f42ffda3490ff66c1e4904fcb661401bc4
BonSeung <bs.koo@sqisoft.com>
2024-10-17 11:08:12 +0900

    feat: 채팅방 ID로 메세지 리스트 불러오는 메서드 추가 - #28

--- src/main/java/org/chatchat/chatmessage/domain/repository/ChatMessageRepository.java
+++ src/main/java/org/chatchat/chatmessage/domain/repository/ChatMessageRepository.java
@@ -3,5 +3,9 @@ package org.chatchat.chatmessage.domain.repository;
 import org.chatchat.chatmessage.domain.ChatMessage;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    List<ChatMessage> findByRoomIdOrderBySentAtDesc(String roomId);
 }
