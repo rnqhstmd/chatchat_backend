3fff6f84b8b270c3d97f51ad5066ea0867a5dfa3
BonSeung <bs.koo@sqisoft.com>
2024-10-23 09:05:18 +0900

    feat: 채팅 메세지 Dto KafkaMessage로 생성 로직 추가 - #50

--- src/main/java/org/chatchat/chatmessage/dto/response/MessageResponse.java
+++ src/main/java/org/chatchat/chatmessage/dto/response/MessageResponse.java
@@ -1,6 +1,7 @@
 package org.chatchat.chatmessage.dto.response;
 
 import org.chatchat.chatmessage.domain.ChatMessage;
+import org.chatchat.kafka.domain.KafkaMessage;
 
 import java.time.format.DateTimeFormatter;
 
@@ -11,13 +12,23 @@ public record MessageResponse(
         String content,
         String sentAt
 ) {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM-dd HH:mm");
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
-    public static MessageResponse from(ChatMessage message) {
+    public static MessageResponse fromChatMessage(ChatMessage message) {
         return new MessageResponse(
                 message.getId(),
                 message.getRoomId(),
                 message.getSender(),
+                message.getContent(),
+                FORMATTER.format(message.getSentAt())
+        );
+    }
+
+    public static MessageResponse fromKafkaMessage(KafkaMessage message) {
+        return new MessageResponse(
+                message.getId(),
+                message.getRoomId(),
+                message.getSenderName(),
                 message.getContent(),
                 FORMATTER.format(message.getSentAt())
         );
