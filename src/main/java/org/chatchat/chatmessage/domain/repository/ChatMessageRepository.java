dcaa27a454ffec4dca7f3c2a2f539b315c0b5082
BonSeung <bs.koo@sqisoft.com>
2024-10-30 22:00:09 +0900

    feat: 특정 채팅방 가장 최근 메세지 조회 메서드 추가 - #53

--- src/main/java/org/chatchat/chatmessage/domain/repository/ChatMessageRepository.java
+++ src/main/java/org/chatchat/chatmessage/domain/repository/ChatMessageRepository.java
@@ -3,10 +3,17 @@ package org.chatchat.chatmessage.domain.repository;
 import org.chatchat.chatmessage.domain.ChatMessage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 
     Page<ChatMessage> findByRoomIdOrderBySentAtDesc(String roomId, Pageable pageable);
+
+    @Query(value = "{ 'roomId' : ?0 }", sort = "{ 'sentAt' : -1 }")
+    List<ChatMessage> findLatestMessageByRoomId(String roomId, Pageable pageable);
 }
