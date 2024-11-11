754367a82bb323ce5d7cb11097df97efecd38817
BonSeung <bs.koo@sqisoft.com>
2024-11-11 21:11:59 +0900

    feat: 읽지 않은 메세지 수 필드 추

--- src/main/java/org/chatchat/chatmessage/dto/response/MessageResponse.java
+++ src/main/java/org/chatchat/chatmessage/dto/response/MessageResponse.java
@@ -1,36 +1,40 @@
 package org.chatchat.chatmessage.dto.response;
 
+import lombok.Builder;
 import org.chatchat.chatmessage.domain.ChatMessage;
 import org.chatchat.kafka.domain.KafkaChatMessage;
 
 import java.time.format.DateTimeFormatter;
 
+@Builder
 public record MessageResponse(
         String id,
         String roomId,
         String sender,
         String content,
-        String sentAt
+        String sentAt,
+        int unreadCount // 읽지 않은 메시지 수 필드 추가
 ) {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
 
-    public static MessageResponse fromChatMessage(ChatMessage message) {
+    public static MessageResponse fromChatMessage(ChatMessage message, int unreadCount) {
         return new MessageResponse(
                 message.getId(),
                 message.getRoomId(),
                 message.getSender(),
                 message.getContent(),
-                FORMATTER.format(message.getSentAt())
+                FORMATTER.format(message.getSentAt()),
+                unreadCount
         );
     }
 
     public static MessageResponse fromKafkaMessage(KafkaChatMessage message) {
-        return new MessageResponse(
-                message.getId(),
-                message.getRoomId(),
-                message.getSenderName(),
-                message.getContent(),
-                String.valueOf(message.getSentAt())
-        );
+        return MessageResponse.builder()
+                .id(message.getId())
+                .roomId(message.getRoomId())
+                .sender(message.getSenderName())
+                .content(message.getContent())
+                .sentAt(String.valueOf(message.getSentAt()))
+                .build();
     }
 }
