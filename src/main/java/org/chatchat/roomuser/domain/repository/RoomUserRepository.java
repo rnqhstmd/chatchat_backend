6fbdaf242d8a7cd7320ee9c3bb03e0391e1b9a14
BonSeung <bs.koo@sqisoft.com>
2024-11-11 21:53:29 +0900

    feat: 읽지 않는 사용자 수 세는 JPA 메서드 추가 - #53

--- src/main/java/org/chatchat/roomuser/domain/repository/RoomUserRepository.java
+++ src/main/java/org/chatchat/roomuser/domain/repository/RoomUserRepository.java
@@ -2,6 +2,8 @@ package org.chatchat.roomuser.domain.repository;
 
 import org.chatchat.roomuser.domain.RoomUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,7 @@ public interface RoomUserRepository extends JpaRepository<RoomUser, Long> {
     Optional<RoomUser> findByRoomIdAndUserId(Long roomId, Long userId);
 
     List<RoomUser> findByRoomId(Long roomId);
+
+    @Query("SELECT COUNT(ru) FROM RoomUser ru WHERE ru.room.id = :roomId AND ru.lastReadMessageId < :messageId")
+    Long countUnreadUsers(@Param("roomId") Long roomId, @Param("messageId") String messageId);
 }
