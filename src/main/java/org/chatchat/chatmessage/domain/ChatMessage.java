bda2b402036156a4e74254f32627eeb2cb246ce1
BonSeung <bs.koo@sqisoft.com>
2024-11-11 21:51:45 +0900

    feat: 사용하지 않는 임포트 제거 - #53

--- src/main/java/org/chatchat/chatmessage/domain/ChatMessage.java
+++ src/main/java/org/chatchat/chatmessage/domain/ChatMessage.java
@@ -7,8 +7,6 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 
 @Getter
 @Document(collection = "chat_messages")
@@ -23,7 +21,6 @@ public class ChatMessage {
     private String sender;
     private String content;
     private LocalDateTime sentAt;
-    private Set<String> readByUsers = new HashSet<>();  // 메시지를 읽은 사용자의 ID 목록
 
     public ChatMessage(MessageType type, String roomId, String sender, String content, LocalDateTime sentAt) {
         this.type = type;
@@ -31,9 +28,5 @@ public class ChatMessage {
         this.sender = sender;
         this.content = content;
         this.sentAt = sentAt;
-    }
-
-    public void markAsRead(String userId) {
-        this.readByUsers.add(userId);
     }
 }
