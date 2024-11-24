3ddccec4891c63ee101ba0bed10fbdd0afe11817
BonSeung <bs.koo@sqisoft.com>
2024-11-24 16:17:22 +0900

    feat: NotificationController 추가 - #63

--- src/main/java/org/chatchat/notification/controller/NotificationController.java
+++ src/main/java/org/chatchat/notification/controller/NotificationController.java
@@ -1,11 +1,24 @@
 package org.chatchat.notification.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.chatchat.notification.util.EmitterService;
+import org.chatchat.security.auth.annotation.AuthUser;
+import org.chatchat.user.domain.User;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
+
+    private final EmitterService emitterService;
+
+    @GetMapping(value = "/subscribe", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter subscribe(@AuthUser User user) {
+        return emitterService.subscribe(user.getId().toString());
+    }
 }
