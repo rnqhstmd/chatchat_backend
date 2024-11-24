ce56a24791dd79cc5993201f84293d0a6131aa91
BonSeung <bs.koo@sqisoft.com>
2024-11-24 15:40:27 +0900

    feat: Notification 생성자 추가 - #63

---
+++ src/main/java/org/chatchat/notification/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package org.chatchat.notification.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+}
