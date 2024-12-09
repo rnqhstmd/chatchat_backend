fe761fb8875a6199cfe1fd55d9034acc1abeeee7
BonSeung <bs.koo@sqisoft.com>
2024-12-09 21:16:37 +0900

    feat: SSE로 알림 전송 시 DTO 사용하게끔 변경 - #63

--- src/main/java/org/chatchat/notification/service/NotificationService.java
+++ src/main/java/org/chatchat/notification/service/NotificationService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.chatchat.kafka.domain.KafkaChatMessage;
 import org.chatchat.notification.domain.Notification;
 import org.chatchat.notification.domain.repository.NotificationRepository;
+import org.chatchat.notification.dto.response.NotificationResponseDto;
 import org.chatchat.notification.util.EmitterService;
 import org.chatchat.room.domain.Room;
 import org.chatchat.room.service.RoomQueryService;
@@ -24,7 +25,7 @@ public class NotificationService {
     private final EmitterService emitterService;
 
     @Transactional
-    public void createAndSendNotification(KafkaChatMessage kafkaChatMessage) {
+    public void processNotification(KafkaChatMessage kafkaChatMessage) {
         Room room = roomQueryService.findExistingRoomByRoomId(Long.valueOf(kafkaChatMessage.getRoomId()));
         List<RoomUser> roomUsers = roomUserQueryService.findExistingRoomUserListByRoomId(Long.valueOf(kafkaChatMessage.getRoomId()));
 
@@ -34,15 +35,20 @@ public class NotificationService {
                 .forEach(user -> {
                     // 알림 생성
                     Notification notification = new Notification(
-                            user.getId().toString(),
-                            kafkaChatMessage.getSenderName(),
+                            user,
                             room.getName(),
                             kafkaChatMessage.getContent(),
                             kafkaChatMessage.getSentAt()
                     );
+
+                    // 알림 저장
                     notificationRepository.save(notification);
+
                     // SSE 로 실시간 알림 전송
-                    emitterService.notify(user.getId().toString(), notification);
+                    emitterService.notify(
+                            user.getId().toString(),
+                            NotificationResponseDto.from(notification)
+                    );
                 });
     }
 }
